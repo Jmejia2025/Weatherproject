14fcee0ddc04bd08eaf9f99d67dac2d4464345f4
Jmejia2025 <jmejia41@mail.valenciacollege.edu>
2025-11-02 09:28:46 -0500

    1st commit

---
+++ Weather severity.py
@@ -0,0 +1,20 @@
+total_rain = 0
+total_wind = 0
+count = 0
+
+while True:
+    data = input()
+    rain, wind = map (float, data.split())
+    if rain == -1.0 and wind == 0:
+        break
+    total_rain += rain
+    total_wind += wind
+    count += 1
+avg_rain = total_rain / count
+avg_wind = total_wind / count
+
+weather_severity = (avg_rain * 10) + avg_wind
+
+print(f"The weather rain is {avg_rain:1f} inches")
+print(f"The weather wind is {avg_wind:1f} mph")
+print(f"The weather severity is for these {count} readings is {weather_severity:.1f} %")
